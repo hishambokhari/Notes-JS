7cc311477d543d859c754790fba58c9b9a00e714
Student <student@Makerss-MacBook-Pro.local>
2020-01-06 17:15:06 +0000

    step 1

---
+++ spec/note-spec.js
@@ -0,0 +1,6 @@
+function noteObjectAcceptsTextAsAnArgument() {
+    var note = new Note('My favourite language is JavaScript.');
+    assert.isTrue(note.text === 'My favourite language is JavaScript.');
+  };
+
+  noteObjectAcceptsTextAsAnArgument()
