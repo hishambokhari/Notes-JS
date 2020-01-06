9fa4afe9b77742b6551e48af60cd998610cec1e8
Student <student@Makerss-MacBook-Pro.local>
2020-01-06 17:29:01 +0000

    step 3

--- spec/note-list-model-spec.js
+++ spec/note-list-model-spec.js
@@ -4,3 +4,11 @@ function noteListIsEmpty() {
 };
 
 noteListIsEmpty()
+
+function noteListWithSingleNotes() {
+    var notelist = new NoteList();
+    notelist.create('my favourite language is ruby');
+    assert.isTrue(notelist.all.length === 1);
+};
+
+noteListWithSingleNotes()
--- src/note-list-model.js
+++ src/note-list-model.js
@@ -3,5 +3,10 @@
         this.all = [];
     }
 
+    NoteList.prototype.create = function(notetext){
+        var note = new Note(notetext);
+        this.all.push(note);
+    }
+
     exports.NoteList = NoteList;
 })(this);
